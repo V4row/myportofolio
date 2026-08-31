13c4317884f19057a985279d8f9633b643d996db
V4row <vnuw12@gmail.com>
2026-08-31 13:23:46 +0700

    Changed the django congiguration name to 'portofolio'

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportofolio.urls'
+ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportofolio.wsgi.application'
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 
 # Database
